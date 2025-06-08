47607012be4999d70c05f2ace83f79cd6323d32d
Manish Patadiya <patadiyamanish20@gmail.com>
2025-06-08 23:14:59 +0530

    first commit

---
+++ model/project.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const proSchema = mongoose.Schema(
+  {
+    project_name: {
+      type: String,
+      required: [true, "Project Name is Required"],
+      trim: true,
+      unique: true,
+    },
+    total_task: {
+      type: Number,
+      default: 0,
+    },
+    pro_ref: {
+      type: String,
+      trim: true
+      // ref: "signup",
+      // required: [true, "Password is Required"],
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "signup"
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+const project_tbl = mongoose.model("project", proSchema);
+
+module.exports = project_tbl;
---
+++ router/auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { handleSignup, handleSignin,handleLogout, handleVerifyOtp, getAllLogedInUser, handleverifyEmailAndSendOtp, handleverifyforgototp, handlechangepassword } = require("../controller/auth");
+
+const router = express.Router();
+router.post("/signup", (req, res) => {
+  handleSignup(req, res);
+});
+router.get("/", async (req, res) => {
+  return res.json({ success: true })
+});
+router.get("/users", async (req, res) => {
+  getAllLogedInUser(req, res)
+});
+router.post("/signin", async (req, res) => {
+  handleSignin(req, res);
+});
+
+// new user singed verify otp
+router.post("/verifyotp", async (req, res) => {
+  handleVerifyOtp(req, res);
+});
+// verify email and sent otp
+router.post("/sendforgototp", async (req, res) => {
+  handleverifyEmailAndSendOtp(req, res);
+  // verifyEmailAndSendOtp
+});
+router.post("/verifyforgototp", async (req, res) => {
+  handleverifyforgototp(req, res);
+});
+router.post("/forgotpassword", async (req, res) => {
+  handlechangepassword(req, res);
+});
+router.get("/logout",(req,res)=>{
+  handleLogout(req,res)
+})
+module.exports = router;
